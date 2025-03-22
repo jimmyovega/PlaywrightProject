a4ee7e231926110794b564fbdfe6947959ef42ef
Jimmy Vega <ikarilink@gmail.com>
2025-03-22 18:46:20 -0500

    Changed Solution file to be asynch as Python Asyncio requires asynchronous files to execute

--- Solutions.py
+++ Solutions.py
@@ -1,52 +1,52 @@
 import pytest
+import pytest_asyncio
 
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 
-@pytest.fixture
-def chromium_browser():
+@pytest_asyncio.fixture
+async def chromium_browser():
     #Setup the browser and return the page object
-    playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
+        context = await browser.new_context()
+        page = await context.new_page()
 
-    #Yield page object to the test function
-    yield page
+        #Yield page object to the test function
+        yield page
 
-    #Teardown steps
-    context.close()
-    browser.close()
-    playwright.stop()
+        #Teardown steps
+        await context.close()
+        await browser.close()
 
 #Step functions
 
-def navigate_to_page(page):
+async def navigate_to_page(page):
     #Navigate to the specific page
-    page.goto("https://leetcode.com/problems/can-place-flowers/")
-    page.wait_for_load_state("networkidle")
+    await page.goto("https://leetcode.com/problems/can-place-flowers/")
+    await page.wait_for_load_state("networkidle")
 
-def sign_in(page):
+async def sign_in(page):
     #Sign in to the LeetCode account
-    page.get_by_role("link", name="Sign in").click()
-    page.get_by_role("textbox", name="Username or E-mail").click()
-    page.get_by_role("textbox", name="Username or E-mail").fill("ikarilink")
-    page.get_by_role("textbox", name="Password").click()
-    page.get_by_role("textbox", name="Password").fill("kEJ!IHLWyijt3vjw7onC")
+    await page.get_by_role("link", name="Sign in").click()
+    await page.get_by_role("textbox", name="Username or E-mail").click()
+    await page.get_by_role("textbox", name="Username or E-mail").fill("ikarilink")
+    await page.get_by_role("textbox", name="Password").click()
+    await page.get_by_role("textbox", name="Password").fill("kEJ!IHLWyijt3vjw7onC")
 
-    page.wait_for_timeout(7000)
+    await page.wait_for_timeout(7000)
     #iframe = page.frame_locator("iframe[title='Widget containing a Cloudflare security challenge']")
-    page.locator(".container__1Q_2 > div > div").first.click()
+    await page.locator(".container__1Q_2 > div > div").first.click()
     #page.locator("iframe[src=\"https\\:\\/\\/challenges\\.cloudflare\\.com\\/cdn-cgi\\/challenge-platform\\/h\\/b\\/turnstile\\/if\\/ov2\\/av0\\/rcv\\/wm0s0\\/0x4AAAAAAAQrSHUTor4iGTpW\\/light\\/fbE\\/new\\/normal\\/en\\/\"]").content_frame.locator("body").click()
 
     # Click the Sign In button
-    page.get_by_role("button", name="Sign In").click()
+    await page.get_by_role("button", name="Sign In").click()
 
-def select_language(page):
+async def select_language(page):
     #Select the language for the solution
-    page.get_by_text("C++").click()
-    page.get_by_text("Python", exact=True).click()
+    await page.get_by_text("C++").click()
+    await page.get_by_text("Python", exact=True).click()
 
-def insert_code(page):
+async def insert_code(page):
     #Insert the code into the code editor
     code = """
 count = 0
@@ -60,50 +60,43 @@ flowerbed[i] = 1
 count += 1
 if count >= n:
 """
-    page.locator(".view-lines > div:nth-child(8)").click()
-    page.keyboard.type(code)
-    page.keyboard.type("return True")
-    page.keyboard.press("Enter")
+    await page.locator(".view-lines > div:nth-child(8)").click()
+    await page.keyboard.type(code)
+    await page.keyboard.type("return True")
+    await page.keyboard.press("Enter")
     for i in range(4):
-        page.keyboard.press("Backspace")
-    page.keyboard.type("return False")
+        await page.keyboard.press("Backspace")
+    await page.keyboard.type("return False")
 
-def run_code(page):
+async def run_code(page):
     #Run the code and return the output
-    page.get_by_role("button", name="Run").click()
-    page.wait_for_selector('div[data-e2e-locator="console-result"]')
-
-def assert_output(page):
-    output = page.locator('div[data-e2e-locator="console-result"]')
-    output.get_by_text("Accepted")
-
-def close_browser(playwright, browser, context):
-    #Close the browser
-    context.close()
-    browser.close()
-    playwright.stop()
+    await page.get_by_role("button", name="Run").click()
+    await page.wait_for_selector('div[data-e2e-locator="console-result"]')
 
 #Test functions
 
-def test_can_navigate_to_leetcode(chromium_browser):
+@pytest.mark.asyncio
+async def test_can_navigate_to_leetcode(chromium_browser):
     #Navigate to the LeetCode website
-    navigate_to_page(chromium_browser)
-    assert chromium_browser.title() == "Can Place Flowers - LeetCode"
+    await navigate_to_page(chromium_browser)
+    assert await chromium_browser.title() == "Can Place Flowers - LeetCode"
 
-def test_can_sign_in(chromium_browser):
+@pytest.mark.asyncio
+async def test_can_sign_in(chromium_browser):
     #Test the sign in process
-    navigate_to_page(chromium_browser)
-    sign_in(chromium_browser)
-    chromium_browser.get_by_role("button", name="avatar").click()
-    user_profile_link = chromium_browser.get_by_role("link", name="ikarilink").inner_text()
+    await navigate_to_page(chromium_browser)
+    await sign_in(chromium_browser)
+    await chromium_browser.get_by_role("button", name="avatar").click()
+    user_profile_link = await chromium_browser.get_by_role("link", name="ikarilink").inner_text()
     assert "ikarilink" in user_profile_link
 
-def test_can_place_flowers(chromium_browser):
+@pytest.mark.asyncio
+async def test_can_place_flowers(chromium_browser):
     #Test the entire process of signing in, typing in the solution and then get an expected Accepted result upon running the code
-    navigate_to_page(chromium_browser)
-    sign_in(chromium_browser)
-    select_language(chromium_browser)
-    insert_code(chromium_browser)
-    run_code(chromium_browser)
-    result = chromium_browser.locator('div[data-e2e-locator="console-result"]').inner_text()
+    await navigate_to_page(chromium_browser)
+    await sign_in(chromium_browser)
+    await select_language(chromium_browser)
+    await insert_code(chromium_browser)
+    await run_code(chromium_browser)
+    result = await chromium_browser.locator('div[data-e2e-locator="console-result"]').inner_text()
     assert "Accepted" in result
